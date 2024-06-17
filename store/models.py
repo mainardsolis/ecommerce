79288711905b79cc7dbef391a6fa6aa8e7d3ceac
mainard <mainardsolis@gmail.com>
2024-06-16 22:45:08 -0300

    Cambios en el front

--- store/models.py
+++ store/models.py
@@ -31,6 +31,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+
+    #agrego cosas del descuento de los productos
     is_sale = models.BooleanField(default=False)
     sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
